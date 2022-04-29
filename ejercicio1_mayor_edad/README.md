f5622d9f02e2e414b4b4601d7b5ab4cde3ae98dd
lisethcorrea <lisethcorrea@colegioguanenta.edu.co>
2022-04-29 06:48:37 -0500

    Desarrollo: diagrama y programa realizados

---
+++ ejercicio1_mayor_edad/README.md
@@ -0,0 +1,7 @@
+#ejercicio 1:
+
+## programa para verificar si una persona es mayor de edad 
+
+## diseño
+
+![diagrama de flujo](diagrama.png)" diagrama de flujo"
---
+++ ejercicio1_mayor_edad/mayor_edad.py
@@ -0,0 +1,15 @@
+"""ejercicio 1: 
+programa para verificar si una persona es mayor de edad"""
+
+print("---------------------------")
+print("--------MAYOR DE EDAD------")
+print("---------------------------")
+
+# input
+edad=int(input("digite la edad: "))
+
+# procesing
+if edad >=18:
+    print( "la persona es MAYOR DE EDAD" )
+else:
+    print("la persona es MENOR DE EDAD")
